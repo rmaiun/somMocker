543903bc0d789d879eb5ad5574ba178a4c92748a
Roman Maiun <roman.maiun@unicorn.com>
2022-10-27 11:08:48 +0300

    fix test

--- src/main/scala/dev/rmaiun/sommocker/Boot.scala
+++ src/main/scala/dev/rmaiun/sommocker/Boot.scala
@@ -6,6 +6,8 @@ import dev.rmaiun.sommocker.services.{ ConfigProvider, RabbitInitializer, Reques
 import zio._
 import zio.logging.backend._
 
+import scala.language.postfixOps
+
 object Boot extends ZIOApp {
 
   private val logger = Runtime.removeDefaultLoggers >>> SLF4J.slf4j
@@ -15,33 +17,38 @@ object Boot extends ZIOApp {
   override type Environment = AppEnv
 
   val refLayer: ZLayer[Scope, Nothing, Ref[Map[ConfigurationKeyDto, ConfigurationDataDto]]] =
-    ZIO.logInfo(s"---> Creating refLayer") *>
-      ZLayer.fromZIO(Ref.make(Map[ConfigurationKeyDto, ConfigurationDataDto]()))
+    ZLayer.fromZIO(
+      ZIO.logInfo(s"---> Creating refLayer") *>
+        Ref.make(Map[ConfigurationKeyDto, ConfigurationDataDto]())
+    )
 
   val structsLayer: ZLayer[Scope, Throwable, AlgorithmStructureSet] = {
-    ZIO.logInfo(s"---> Creating structsLayer") *>
-      ZLayer.fromZIO {
-        val appCfg        = ConfigProvider.provideConfig
-        val algorithmCode = appCfg.broker.algorithm
-        val a1            = algorithmCode.replace("$$$", "1")
-        val a2            = algorithmCode.replace("$$$", "2")
-        val a3            = algorithmCode.replace("$$$", "3")
-        val a4            = algorithmCode.replace("$$$", "4")
-        for {
-          structsAlg1 <- RabbitInitializer.initRabbit(a1, appCfg.broker)
-          structsAlg2 <- RabbitInitializer.initRabbit(a2, appCfg.broker)
-          structsAlg3 <- RabbitInitializer.initRabbit(a3, appCfg.broker)
-          structsAlg4 <- RabbitInitializer.initRabbit(a4, appCfg.broker)
-        } yield AlgorithmStructureSet(
-          Set(
-            AlgorithmStructure(a1, structsAlg1),
-            AlgorithmStructure(a2, structsAlg2),
-            AlgorithmStructure(a3, structsAlg3),
-            AlgorithmStructure(a4, structsAlg4)
-          )
+    ZLayer.fromZIO {
+      val appCfg        = ConfigProvider.provideConfig
+      val algorithmCode = appCfg.broker.algorithm
+      val a1            = algorithmCode.replace("$$$", "1")
+      val a2            = algorithmCode.replace("$$$", "2")
+      val a3            = algorithmCode.replace("$$$", "3")
+      val a4            = algorithmCode.replace("$$$", "4")
+      val effect = for {
+        _           <- ZIO.logInfo(s"---> Creating structsLayer")
+        structsAlg1 <- RabbitInitializer.initRabbit(a1, appCfg.broker)
+        structsAlg2 <- RabbitInitializer.initRabbit(a2, appCfg.broker)
+        structsAlg3 <- RabbitInitializer.initRabbit(a3, appCfg.broker)
+        structsAlg4 <- RabbitInitializer.initRabbit(a4, appCfg.broker)
+      } yield AlgorithmStructureSet(
+        Set(
+          AlgorithmStructure(a1, structsAlg1),
+          AlgorithmStructure(a2, structsAlg2),
+          AlgorithmStructure(a3, structsAlg3),
+          AlgorithmStructure(a4, structsAlg4)
         )
-      }
-        .tapErrorCause(err => ZIO.logCause(Cause.fail(err)))
+      )
+      for {
+        structs <- effect.tapErrorCause(err => ZIO.logCause(Cause.fail(err)))
+        _       <- ZIO.logInfo(s"---> Structs were successfully created")
+      } yield structs
+    }
   }
 
   override def bootstrap: ZLayer[ZIOAppArgs, Any, Environment] = {
@@ -56,8 +63,9 @@ object Boot extends ZIOApp {
   }
 
   override def run: ZIO[Environment & ZIOAppArgs, Any, Any] =
-    Server
-      .run()
-      .tapErrorCause(err => ZIO.logCause(Cause.fail(err)))
-      .exitCode
+    ZIO.logInfo(s"---> Starting server") *>
+      Server
+        .start()
+        .tapErrorCause(err => ZIO.logCause(Cause.fail(err)))
+        .exitCode
 }
--- src/main/scala/dev/rmaiun/sommocker/Server.scala
+++ src/main/scala/dev/rmaiun/sommocker/Server.scala
@@ -1,9 +1,9 @@
 package dev.rmaiun.sommocker
 
-import cats.effect.{ Async, Resource }
+import cats.effect.{Async, Resource}
 import cats.syntax.all._
 import com.comcast.ip4s._
-import dev.rmaiun.sommocker.dtos.{ AlgorithmStructureSet, ConfigurationDataDto, ConfigurationKeyDto }
+import dev.rmaiun.sommocker.dtos.{AlgorithmStructureSet, ConfigurationDataDto, ConfigurationKeyDto}
 import dev.rmaiun.sommocker.services.RequestProcessor
 import org.http4s.ember.server.EmberServerBuilder
 import org.http4s.implicits._
@@ -12,29 +12,30 @@ import org.http4s.server.middleware.Logger
 import sttp.tapir.server.http4s.ztapir.ZHttp4sServerInterpreter
 import sttp.tapir.swagger.bundle.SwaggerInterpreter
 import zio.interop.catz._
-import zio.{ RIO, _ }
+import zio.{RIO, _}
 
 object Server {
   type MockRef = Ref[Map[ConfigurationKeyDto, ConfigurationDataDto]]
   type AppEnv  = RequestProcessor with AlgorithmStructureSet with MockRef with Scope
-  def run(): ZIO[AppEnv, Throwable, Unit] = {
+  def start(): ZIO[AppEnv, Throwable, Unit] =
     // docs/index.html?url=/docs/docs.yml
-    val swaggerRoutes = ZHttp4sServerInterpreter()
-      .from(SwaggerInterpreter().fromServerEndpoints[RIO[AppEnv, *]](Endpoints.endpoints, "sommocker", "0.1.0"))
-      .toRoutes
-    val httpApp      = Router("/" -> (Endpoints.routes <+> swaggerRoutes)).orNotFound
-    val finalHttpApp = Logger.httpApp(logHeaders = true, logBody = false)(httpApp)
     for {
-      _   <- ZIO.logInfo("Starting server ...")
       set <- ZIO.service[AlgorithmStructureSet]
       rp  <- ZIO.service[RequestProcessor]
+      _   <- ZIO.logInfo("Starting request consumer ...")
       _   <- (ZIO foreach set.structures)(s => s.structs.requestConsumer.tap(str => rp.processIncomingMessage(str)).runDrain.fork)
+      _   <- ZIO.logInfo("request consumer is successfully started")
+      swaggerRoutes = ZHttp4sServerInterpreter()
+                        .from(SwaggerInterpreter().fromServerEndpoints[RIO[AppEnv, *]](Endpoints.endpoints, "sommocker", "0.1.0"))
+                        .toRoutes
+      httpApp      = Router("/" -> (Endpoints.routes <+> swaggerRoutes)).orNotFound
+      finalHttpApp = Logger.httpApp(logHeaders = true, logBody = false)(httpApp)
       scoped <- (EmberServerBuilder
                   .default[RIO[AppEnv, *]]
                   .withHost(ipv4"0.0.0.0")
                   .withPort(port"8080")
                   .withHttpApp(finalHttpApp)
                   .build >> Resource.eval(Async[RIO[AppEnv, *]].never)).toScopedZIO
+                  .tapErrorCause(err => ZIO.logCause(Cause.fail(err)))
     } yield scoped
-  }
 }
