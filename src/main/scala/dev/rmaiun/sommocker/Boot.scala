21bc043bca68fad4f1636578ad7232632ab4ee56
Roman Maiun <roman.maiun@unicorn.com>
2022-10-27 10:59:54 +0300

    more error logging

--- src/main/scala/dev/rmaiun/sommocker/Boot.scala
+++ src/main/scala/dev/rmaiun/sommocker/Boot.scala
@@ -15,40 +15,45 @@ object Boot extends ZIOApp {
   override type Environment = AppEnv
 
   val refLayer: ZLayer[Scope, Nothing, Ref[Map[ConfigurationKeyDto, ConfigurationDataDto]]] =
-    ZLayer.fromZIO(Ref.make(Map[ConfigurationKeyDto, ConfigurationDataDto]()))
+    ZIO.logInfo(s"---> Creating refLayer") *>
+      ZLayer.fromZIO(Ref.make(Map[ConfigurationKeyDto, ConfigurationDataDto]()))
 
   val structsLayer: ZLayer[Scope, Throwable, AlgorithmStructureSet] = {
-    ZLayer.fromZIO {
-      val appCfg        = ConfigProvider.provideConfig
-      val algorithmCode = appCfg.broker.algorithm
-      val a1            = algorithmCode.replace("$$$", "1")
-      val a2            = algorithmCode.replace("$$$", "2")
-      val a3            = algorithmCode.replace("$$$", "3")
-      val a4            = algorithmCode.replace("$$$", "4")
-      for {
-        structsAlg1 <- RabbitInitializer.initRabbit(a1, appCfg.broker)
-        structsAlg2 <- RabbitInitializer.initRabbit(a2, appCfg.broker)
-        structsAlg3 <- RabbitInitializer.initRabbit(a3, appCfg.broker)
-        structsAlg4 <- RabbitInitializer.initRabbit(a4, appCfg.broker)
-      } yield AlgorithmStructureSet(
-        Set(
-          AlgorithmStructure(a1, structsAlg1),
-          AlgorithmStructure(a2, structsAlg2),
-          AlgorithmStructure(a3, structsAlg3),
-          AlgorithmStructure(a4, structsAlg4)
+    ZIO.logInfo(s"---> Creating structsLayer") *>
+      ZLayer.fromZIO {
+        val appCfg        = ConfigProvider.provideConfig
+        val algorithmCode = appCfg.broker.algorithm
+        val a1            = algorithmCode.replace("$$$", "1")
+        val a2            = algorithmCode.replace("$$$", "2")
+        val a3            = algorithmCode.replace("$$$", "3")
+        val a4            = algorithmCode.replace("$$$", "4")
+        for {
+          structsAlg1 <- RabbitInitializer.initRabbit(a1, appCfg.broker)
+          structsAlg2 <- RabbitInitializer.initRabbit(a2, appCfg.broker)
+          structsAlg3 <- RabbitInitializer.initRabbit(a3, appCfg.broker)
+          structsAlg4 <- RabbitInitializer.initRabbit(a4, appCfg.broker)
+        } yield AlgorithmStructureSet(
+          Set(
+            AlgorithmStructure(a1, structsAlg1),
+            AlgorithmStructure(a2, structsAlg2),
+            AlgorithmStructure(a3, structsAlg3),
+            AlgorithmStructure(a4, structsAlg4)
+          )
         )
-      )
-    }
-      .tapErrorCause(err => ZIO.logCause(Cause.fail(err)))
+      }
+        .tapErrorCause(err => ZIO.logCause(Cause.fail(err)))
   }
 
-  override def bootstrap: ZLayer[ZIOAppArgs, Any, Environment] = ZLayer
-    .make[Environment](
-      Scope.default,
-      refLayer,
-      structsLayer,
-      RequestProcessor.live
-    ) ++ logger
+  override def bootstrap: ZLayer[ZIOAppArgs, Any, Environment] = {
+    val finalLayer = ZLayer
+      .make[Environment](
+        Scope.default,
+        refLayer,
+        structsLayer,
+        RequestProcessor.live
+      ) ++ logger
+    finalLayer.tapErrorCause(err => ZIO.logCause(Cause.fail(err)))
+  }
 
   override def run: ZIO[Environment & ZIOAppArgs, Any, Any] =
     Server
